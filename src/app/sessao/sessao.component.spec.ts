8f98031b0c872d3d7c196593d2886c32c0581876
yasfreitasGitHub <yasmin.freitas1707@gmail.com>
2023-11-09 10:03:17 -0300

    Projeto Loja

---
+++ src/app/sessao/sessao.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SessaoComponent } from './sessao.component';
+
+describe('SessaoComponent', () => {
+  let component: SessaoComponent;
+  let fixture: ComponentFixture<SessaoComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SessaoComponent]
+    });
+    fixture = TestBed.createComponent(SessaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sessao/sessao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sessao',
+  templateUrl: './sessao.component.html',
+  styleUrls: ['./sessao.component.scss']
+})
+export class SessaoComponent {
+
+}
